9e9990aab8b1c15f25b10a5af759ac6849a1f0d5
guhengliang <tvruanjian05@top-tech.com.cn>
2019-05-13 10:03:37 +0800

    delete object file

--- average.c
+++ average.c
@@ -4,15 +4,15 @@ int main(void)
 	int n,i,score,count=0,s=0;
 	float average;
 	scanf("%d",&n);
-	scanf("%d",$score);
+	scanf("%d",&score);
 	for(i=1;i<=n;i++)
 	{
 		s=s+score;
 		if(score>=60)
 			count++;
-		scanf("%d",$score);
+		scanf("%d",&score);
 	}
 	average=s*1.0/n;
-	printf("average=%.1f",averge);
+	printf("average=%.1f",average);
 	printf("count=%d",count);
 }
